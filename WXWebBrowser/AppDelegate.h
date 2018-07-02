312d52d6c216bed6ea00d805a70bba2ca5310afc
sivanwu <1984430988@qq.com>
2018-07-02 15:27:18 +0800

    占个坑

---
+++ WXWebBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WXWebBrowser
+//
+//  Created by HFY on 2018/7/2.
+//  Copyright © 2018年 wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
